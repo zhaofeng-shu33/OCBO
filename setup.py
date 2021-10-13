097f06f41232378abcfe2b8f0b61b3a42221d982
zhaofeng-shu33 <616545598@qq.com>
2021-10-13 15:08:50 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup, find_packages
+with open("README.md") as fh:
+    long_description = fh.read()
 setup(
     name='OCBO',
     version='0.1.0',
-    packages=find_packages(include=['OCBO', 'OCBO.*'])
+    packages=find_packages(include=['OCBO', 'OCBO.*']),
+    url = 'https://github.com/zhaofeng-shu33/OCBO',
+    author = 'zhaofeng-shu33',
+    author_email = '616545598@qq.com',
+    long_description = long_description,
+    long_description_content_type="text/markdown",           
+    license = 'MIT License',
+    description = 'Offline Contextual Bayesian Optimization'    
 )
