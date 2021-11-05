a8e132f3ab77da4a6f58c630b27bfe7e37cc0380
zhaofeng-shu33 <616545598@qq.com>
2021-11-05 10:21:57 +0800

    make matplotlib necessary

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ with open("README.md") as fh:
     long_description = fh.read()
 setup(
     name='OCBO',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(include=['OCBO', 'OCBO.*']),
     url = 'https://github.com/zhaofeng-shu33/OCBO',
     author = 'zhaofeng-shu33',
     author_email = '616545598@qq.com',
     long_description = long_description,
     long_description_content_type="text/markdown",
-    install_requires=['dragonfly-opt', 'scikit-learn'],
+    install_requires=['dragonfly-opt', 'scikit-learn', 'matplotlib'],
     license = 'MIT License',
     description = 'Offline Contextual Bayesian Optimization'    
 )
